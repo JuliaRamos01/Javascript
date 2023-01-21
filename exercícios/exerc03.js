309b7434f483d2d65cbb3ea4ebeb4870f7f1cfb1
Julia Ramos <julia.jrra@gmail.com>
2023-01-21 19:43:39 -0300

    Atividades do dia 21/01

---
+++ exercícios/exerc03.js
@@ -0,0 +1,9 @@
+var idade = 22 //parei em 12:45
+
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18) {
+    console.log('Voto opcional')
+} else if (idade > 18) {
+    console.log('Voto obrigatório!')
+}
