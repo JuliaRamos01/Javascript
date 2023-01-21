0331376a4251465fe6cb5556662c73d46b80183e
Julia Ramos <julia.jrra@gmail.com>
2023-01-21 18:31:11 -0300

    Exercícios do dia 21/01

--- exercícios/exerc01.js
+++ exercícios/exerc01.js
@@ -1,1 +1,11 @@
 console.log('O console funcionou direitinho!')
+
+var vel = 80
+
+console.log(`A velocidade do carro é ${vel}km/h`)
+
+if (vel > 60){ // isso é uma condição simples
+console.log('Você ultrapassou o limite permitido!')
+}
+
+console.log('Dirija com segurança!')
---
+++ exercícios/exerc02.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Tu é Brasileiro')
+} else{
+    console.log('Tu é Estrangeiro')
+}
